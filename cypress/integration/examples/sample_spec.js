5ce70d85d849e7ceeb8201745288a862abcfde0a
AhmedAGadir <ahmed.gadir@gmail.com>
2020-03-04 12:22:40 +0000

    renders first row test complete

--- cypress/integration/examples/sample_spec.js
+++ cypress/integration/examples/sample_spec.js
@@ -1,59 +1,3 @@
-import { AgGridVue } from 'ag-grid-vue';
-
-// cypress is bundled with mocha and chai
-
-let component = null;
-let agGridVue = null;
-
-const ensureGridApiHasBeenSet = (component) => {
-    return new Promise(function (resolve, reject) {
-        (function waitForGridReady() {
-
-            // we need to wait for the gridReady event before we can start interacting with the grid
-            // in this case we're looking at the api property in our App component, but it could be anything (ie a boolean flag)
-            if (component.vm.gridApi) {
-
-                // once our condition has been met we can start the tests
-                return resolve();
-            }
-            console.log(component.vm.gridApi)
-
-            // not set - wait a bit longer
-            setTimeout(waitForGridReady, 10);
-        })();
-    });
-};
-
-describe('My ag-Grid test', () => {
-    //  basic tests, filtering, sorting, checking content 
-    // of grid data/rows
-
-    beforeEach(done => {
-
-        component = mount(App);
-        agGridVue = component.vm;
-
-        // don't start our tests until the grid is ready
-        // it doesn't take long for the grid to initialise, but it is some finite amount of time after the component is ready
-        ensureGridApiHasBeenSet(component).then(() => done());
-
-        cy.visit('http://localhost:8082');
-        cy.find(AgGridVue).instance()
-        ensureGridApiHasBeenSet().then(() => done());
-    })
-    it('renders the first row', () => {
-        // { make: "Toyota", model: "Celica", price: 35000 },
-
-    });
-    it('sorts the data', () => {
-
-    })
-    it('filters for Toyota', () => {
-
-    });
-})
-
-
 // describe('My first test', function () {
 //     it('Does not do much', function () {
 //         // Arrange - setup initial app state
@@ -77,3 +21,36 @@ describe('My ag-Grid test', () => {
 //             .should('have.value', 'fake@email.com');
 //     });
 // })
+
+function assertCellValueInFirstRow(colId, value) {
+    cy.get('.ag-center-cols-container .ag-row')
+        .first()
+        .find(`[col-id="${colId}"]`)
+        .then(cell => {
+            expect(cell).to.have.text(value);
+        });
+}
+
+describe('My ag-Grid tests', () => {
+    //  basic tests, filtering, sorting, checking content 
+    // of grid data/rows
+    beforeEach(() => {
+        // we don't need to ensure that the grid API is set before running
+        // our tests because Cypress will repeatedly run queries and assertions
+        // until either the element is found or a timeout is reached (4000ms by default)
+        // see https://docs.cypress.io/guides/core-concepts/introduction-to-cypress.html#1-The-element-is-found
+        cy.visit('http://localhost:8082');
+    });
+    it('renders the first row', () => {
+        // { make: "Toyota", model: "Celica", price: 35000 },
+        assertCellValueInFirstRow('make', 'Toyota');
+        assertCellValueInFirstRow('model', 'Celica');
+        assertCellValueInFirstRow('price', '35000');
+    });
+    it('sorts the data', () => {
+
+    })
+    it('filters for Toyota', () => {
+
+    });
+})
